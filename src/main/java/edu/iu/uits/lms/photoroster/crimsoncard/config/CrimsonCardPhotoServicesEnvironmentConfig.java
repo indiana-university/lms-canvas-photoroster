e846755d98680f4411f11cb520141c5bff0cdf9e
chmaurer <chmaurer@iu.edu>
2023-04-27 15:03:30 -0400

    LMSA-8877 - getting the crimsoncard api to work

--- src/main/java/edu/iu/uits/lms/photoroster/crimsoncard/config/CrimsonCardPhotoServicesEnvironmentConfig.java
+++ src/main/java/edu/iu/uits/lms/photoroster/crimsoncard/config/CrimsonCardPhotoServicesEnvironmentConfig.java
@@ -36,20 +36,18 @@ package edu.iu.uits.lms.photoroster.crimsoncard.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizationContext;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
 import org.springframework.util.StringUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -74,7 +72,7 @@ public class CrimsonCardPhotoServicesEnvironmentConfig {
     @Bean
     OAuth2AuthorizedClientManager authorizedClientManager(
             ClientRegistrationRepository clientRegistrationRepository,
-            OAuth2AuthorizedClientRepository authorizedClientRepository) {
+            OAuth2AuthorizedClientService clientService) {
 
         OAuth2AuthorizedClientProvider authorizedClientProvider =
                 OAuth2AuthorizedClientProviderBuilder.builder()
@@ -83,11 +81,11 @@ public class CrimsonCardPhotoServicesEnvironmentConfig {
                         .clientCredentials()
                         .password()
                         .build();
-        DefaultOAuth2AuthorizedClientManager authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
-                clientRegistrationRepository, authorizedClientRepository);
+        AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager =
+                new AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, clientService);
         authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
 
-        // For the `password` grant, the `username` and `password` are supplied via request parameters,
+        // For the `password` grant, the `username` and `password` need to be supplied
         // so map it to `OAuth2AuthorizationContext.getAttributes()`.
         authorizedClientManager.setContextAttributesMapper(contextAttributesMapper());
 
@@ -97,9 +95,8 @@ public class CrimsonCardPhotoServicesEnvironmentConfig {
     private Function<OAuth2AuthorizeRequest, Map<String, Object>> contextAttributesMapper() {
         return authorizeRequest -> {
             Map<String, Object> contextAttributes = Collections.emptyMap();
-            HttpServletRequest servletRequest = authorizeRequest.getAttribute(HttpServletRequest.class.getName());
-            String username = servletRequest.getParameter(OAuth2ParameterNames.USERNAME);
-            String password = servletRequest.getParameter(OAuth2ParameterNames.PASSWORD);
+            String username = config.getClientId();
+            String password = config.getSecret();
             if (StringUtils.hasText(username) && StringUtils.hasText(password)) {
                 contextAttributes = new HashMap<>();
 
