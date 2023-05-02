f92506c4c5239229b2c1e6ef4aadc241e681ad3a
Damian Sobieralski <dsobiera@iu.edu>
2023-05-02 13:19:10 -0400

    LMSA-8877 - updated cloud dep version and added security policy

--- src/main/java/edu/iu/uits/lms/photoroster/config/SecurityConfig.java
+++ src/main/java/edu/iu/uits/lms/photoroster/config/SecurityConfig.java
@@ -43,6 +43,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import uk.ac.ox.ctl.lti13.Lti13Configurer;
 
 import static edu.iu.uits.lms.lti.LTIConstants.BASE_USER_ROLE;
@@ -69,13 +70,13 @@ public class SecurityConfig {
                     .antMatchers("/api/**").permitAll()
                     .antMatchers(WELL_KNOWN_ALL, "/error").permitAll()
                     .antMatchers("/**").hasRole(BASE_USER_ROLE)
-                    .withObjectPostProcessor(new LmsFilterSecurityInterceptorObjectPostProcessor());
-//                    .and()
-//                    .headers()
-//                    .contentSecurityPolicy("form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
-//                    .and()
-//                    .referrerPolicy(referrer -> referrer
-//                            .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN));
+                    .withObjectPostProcessor(new LmsFilterSecurityInterceptorObjectPostProcessor())
+                    .and()
+                    .headers()
+                    .contentSecurityPolicy("style-src 'self' 'unsafe-inline'; form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
+                    .and()
+                    .referrerPolicy(referrer -> referrer
+                            .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN));
 
             //Setup the LTI handshake
             Lti13Configurer lti13Configurer = new Lti13Configurer()
